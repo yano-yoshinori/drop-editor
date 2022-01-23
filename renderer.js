71e226170921f17b6e1ed528472a5feb52bfafa5
Yoshinori Yano <yano@8sai.co.jp>
2022-01-23 21:03:21 +0900

    テキストをドロップできるようになった

--- renderer.js
+++ renderer.js
@@ -4,3 +4,28 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const ta = document.querySelector("textarea");
+ta.style.height = `${window.innerHeight}px`;
+ta.focus();
+
+ta.addEventListener("drop", function (e) {
+  e.preventDefault();
+
+  ta.focus();
+
+  const { dataTransfer } = e;
+  const text = dataTransfer.getData("text/plain");
+  ta.value = ta.value + text + "\n\n";
+
+  const len = ta.value.length;
+
+  ta.selectionStart = 0;
+  ta.selectionEnd = 0;
+
+  setTimeout(function () {
+    // タイマー使わないと反映されない
+    ta.selectionStart = len;
+    ta.selectionEnd = len;
+  });
+});
