1901c714d909bbf8ccf107dc8a70dc9fe57ab6f9
yoshinori yano <yano@8sai.co.jp>
2022-04-02 22:35:30 +0900

    データをファイルに書き込む

--- src/preload.js
+++ src/preload.js
@@ -1,5 +1,10 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+
+const fs = require('fs')
+
+const backupDir = 'storage'
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
@@ -9,9 +14,24 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+
+  if (fs.existsSync(`./${backupDir}/drop-editor.txt`)) {
+    const text = fs.readFileSync(`./${backupDir}/drop-editor.txt`)
+    const ta = document.querySelector('textarea')
+    ta.value = text
+  }
 })
 
 window.addEventListener('beforeunload', () => {
   const ta = document.getElementById('editor')
   window.localStorage.setItem('drop-editor', ta.value)
+
+  // backup
+  if (!fs.existsSync(`./${backupDir}`)) {
+    fs.mkdirSync(`./${backupDir}`)
+  }
+
+  const now = new Date().toISOString()
+  const [date] = now.split('T')
+  fs.writeFileSync(`./${backupDir}/drop-editor-${date}.txt`, ta.value, { encoding: 'utf-8' })
 })
